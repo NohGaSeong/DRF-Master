026c91ba043d6d07491f53dca0aa579e3534e965
yohan <noyohanx@naver.com>
2022-12-05 12:12:49 +0900

    finish chapter 10

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,8 @@
-from todos.views import CreateTodoAPIView, TodoListAPIView
+from todos.views import TodoAPIView
 from django.urls import path
 
 urlpatterns = [
-    path('create', CreateTodoAPIView.as_view(), name = 'create-todo'),
-    path('list', TodoListAPIView.as_view(), name = 'list-todo')
+    # path('create', CreateTodoAPIView.as_view(), name = 'create-todo'),
+    # path('list', TodoListAPIView.as_view(), name = 'list-todo')
+    path("", TodoAPIView.as_view(), name = "todos")
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from todos.serializers import TodoSerializer
 from todos.models import Todo
 
-class CreateTodoAPIView(CreateAPIView):
-    serializer_class = TodoSerializer
-    permission_classes = (IsAuthenticated, )
+# class CreateTodoAPIView(CreateAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = (IsAuthenticated, )
     
-    def perform_create(self, serializer):
-        return serializer.save(owner=self.request.user)
+#     def perform_create(self, serializer):
+#         return serializer.save(owner=self.request.user)
 
 
-class TodoListAPIView(ListAPIView):
+# class TodoListAPIView(ListAPIView):
+#     serializer_class = TodoSerializer
+#     permission_classes = (IsAuthenticated,)
+#     queryset = Todo.objects.all()
+
+#     def get_queryset(self):
+#         return Todo.objects.filter(owner=self.request.user)
+
+
+class TodoAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
-    queryset = Todo.objects.all()
 
-    def get_queryset(self):
-        return Todo.objects.filter(owner=self.request.user)
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
